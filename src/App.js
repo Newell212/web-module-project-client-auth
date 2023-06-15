fef54d379449778bcfcc068f3f14c52f4d0ca43a
Michael Newell <michael.newell212@gmail.com>
2023-06-15 16:42:44 -0600

    WIP

--- src/App.js
+++ src/App.js
@@ -2,11 +2,39 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
+
+const FriendsList = () => {
+  return (<h2>FriendList</h2>)
+}
+
+const AddFriend = () => {
+  return (<h2>AddFriend</h2>)
+}
+
+const Login = () => {
+  return (<h2>Login</h2>)
+}
+
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+   
+      <div className="App">
+        <Router>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/friends">
+            <FriendsList />
+          </Route>
+          <Route exact path="/friends/add">
+            <AddFriend />
+          </Route>
+        </Router>
+      </div>
+    
   );
 }
 
