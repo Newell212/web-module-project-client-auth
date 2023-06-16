779f2a24ddd938f2537f4f1b1c32e4f48214ef41
Michael Newell <michael.newell212@gmail.com>
2023-06-16 17:20:55 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Redirect, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Link, Switch} from 'react-router-dom';
 
 
 import Login from './Components/Login';
 import FriendsList from './Components/FriendsList';
 import AddFriend from './Components/AddFriend';
-
+import Logout from './Components/Logout';
 
 function App() {
   return (
    
       <div className="App">
+        
+        <Router>
         <header>
           <h2>Friends Database</h2>
-          <Link className="link" to="login">Login</Link>
-          <Link className="link" to="friends">Friends List</Link>
-          <Link className="link" to="friends/add">Add Friend</Link>
-          <Link className="link" to="friends">Logout</Link>
+          <Link className="link" to="/login">Login</Link>
+          <Link className="link" to="/friends">Friends List</Link>
+          <Link className="link" to="/friends/add">Add Friend</Link>
+          <Link className="link" to="/logout">Logout</Link>
         </header>
-        <Router>
+          <Switch>
           <Route exact path="/">
             <Login />
           </Route>
@@ -32,6 +34,10 @@ function App() {
           <Route exact path="/friends/add">
             <AddFriend />
           </Route>
+          <Route exact path="/logout">
+            <Logout />
+          </Route>
+          </Switch>
         </Router>
       </div>
     
